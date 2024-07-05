6827a0cd6a0bf129c0004bbffbda9d090cc929b1
Diansavov <diansavov2@abv.bg>
2024-07-05 13:41:00 +0300

    SomeMoreInfo

--- Models/ViewModels/CityTemperature.cs
+++ Models/ViewModels/CityTemperature.cs
@@ -13,7 +13,12 @@ namespace WeatherAspNet.Models.ViewModels
 
             Weather = new Weather();
             Weather.Main = weatherData.Weather[0].Main;
-            Weather.Description = weatherData.Weather[0].Description;
+            Weather.Description = char.ToUpper(weatherData.Weather[0].Description[0]) + weatherData.Weather[0].Description.Substring(1);
+
+            AirInfo = new AirInfo();
+            AirInfo.Humidity = weatherData.Main.Humidity;
+            AirInfo.WindSpeed = weatherData.Wind.Speed;
+            AirInfo.Visibility = Math.Round(weatherData.Visibility / 1000.0, 2);
         }
         public string CityName { get; set; }
         public double CityTemp { get; set; }
@@ -21,10 +26,17 @@ namespace WeatherAspNet.Models.ViewModels
         public double CityTempFeelsLike { get; set; }
         public string IconUrl { get; set; }
         public Weather Weather { get; set; }
+        public AirInfo AirInfo { get; set; }
     }
     public class Weather
     {
         public string Main { get; set; }
         public string Description { get; set; }
+    }
+    public class AirInfo
+    {
+        public double Visibility { get; set; }
+        public int Humidity { get; set; }
+        public float WindSpeed { get; set; }
     }
 }
