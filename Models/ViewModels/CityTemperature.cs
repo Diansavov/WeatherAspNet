fc0a3ebb05b549805782c1bfa802c60159ec90fd
Diansavov <diansavov2@abv.bg>
2024-07-05 12:58:43 +0300

    ExtraInfo

--- Models/ViewModels/CityTemperature.cs
+++ Models/ViewModels/CityTemperature.cs
@@ -6,11 +6,25 @@ namespace WeatherAspNet.Models.ViewModels
         public CityTemperature(WeatherData weatherData)
         {
             CityName = weatherData.Name;
+            CountryCode = weatherData.Sys.Country;
             CityTemp = weatherData.Main.TempCelsius;
+            CityTempFeelsLike = weatherData.Main.Feels_Like - 273.15f;
             IconUrl = "https://openweathermap.org/img/wn/" + weatherData.Weather[0].Icon + ".png";
+
+            Weather = new Weather();
+            Weather.Main = weatherData.Weather[0].Main;
+            Weather.Description = weatherData.Weather[0].Description;
         }
-         public string CityName { get; set; }
-         public double CityTemp { get; set; }
-         public string IconUrl { get; set; }
+        public string CityName { get; set; }
+        public double CityTemp { get; set; }
+        public string CountryCode { get; set; }
+        public double CityTempFeelsLike { get; set; }
+        public string IconUrl { get; set; }
+        public Weather Weather { get; set; }
+    }
+    public class Weather
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
     }
 }
--- Models/WeatherData.cs
+++ Models/WeatherData.cs
@@ -34,13 +34,13 @@ namespace WeatherAspNet.Models
     public class Main
     {
         public float Temp { get; set; }
-        public float FeelsLike { get; set; }
-        public float TempMin { get; set; }
-        public float TempMax { get; set; }
+        public float Feels_Like { get; set; }
+        public float Temp_Min { get; set; }
+        public float Temp_Max { get; set; }
         public int Pressure { get; set; }
         public int Humidity { get; set; }
         public int SeaLevel { get; set; }
-        public int GroundLevel { get; set; }
+        public int GrndLevel { get; set; }
         public float TempKf { get; set; }
 
         // Additional property to get temperature in Celsius
